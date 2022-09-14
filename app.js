b4761a372f76bc00676fb84f62d435cf835c4f2f
Darío Diderich <dari.diderich@gmail.com>
2022-09-14 18:43:38 -0300

    primer entrega del proyecto final

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// Obtener nota de evaluacion oral mediante preguntas.
+
+const preguntas = ["¿De qué nacionalidad era Juana de Arco?", "¿Quién dibujo al famoso Hombre de Vitruvio?", "¿Qué famoso filósofo fue maestro de Alejandro Magno durante cinco años?", "¿Quién fue el último zar de Rusia?", "¿Quienes eran los templarios?", "¿Qué inició la Segunda Guerra Mundial?", "¿Dónde se han encontrado principalmente pinturas del período Paleolítico?", "¿Era Averroes un antiguo filósofo griego?", "¿Cómo comenzó la Primera Guerra Mundial?", "¿En qué año descubrió Cristóbal Colón, América?", "PODRÁ REEVER CADA PREGUNTA Y EL NUMERO EN LA CONSOLA PARA PODER DEFINIR LA NOTA FINAL"]
+
+const mas_preguntas = ["\n \n NUEVAS PREGUNTAS: ", "¿A qué emperador romano asesinó un grupo de senadores?", "¿Quién dijo que la tierra giraba alrededor del sol?", "¿Cuál es el antiguo nombre de San Petersburgo?", "¿Qué famosa batalla perdió y dejó humillado a Napoleón Bonaparte?", "¿Quién fue el primer presidente de los Estados Unidos?"]
+
+const todas_preguntas = preguntas.concat(mas_preguntas)
+
+
+
 // Promedios de alumnos
 
 function pedir_nombres() {
@@ -8,8 +18,10 @@ function pedir_nombres() {
 
 
 function pedir_notas() {
-pedir_nombres();
-let nota1 = Number(prompt ("Ingresar nota de evaluación"));
+
+    pedir_nombres();    
+
+let nota1 = Number(prompt ("Ingresar nota de evaluación oral"));
 console.log(`Evaluación: ${nota1}`)
 
 let nota2 = Number(prompt ("Ingresar nota de TP"));
@@ -29,14 +41,32 @@ console.log(`Promedio: ${promedio_notas}`)
 let alumnos = 1;
 
 
-for(let alumnos = 1; alumnos <= 30 ; alumnos++) {
+
+
+for(let alumnos = 1; alumnos <= 99 ; alumnos++) {
+    for (let ip = 0; ip <= 10; ip++) {
+        alert (preguntas[ip]);
+    }
+    
+    let mas_preguntas = prompt ("¿Necesita mas preguntas?")
+    if (mas_preguntas === "si"){
+      alert(todas_preguntas)
+    }
+    preguntas.forEach((cada_pregunta, indice_pregunta) =>{
+        console.log(`${indice_pregunta}:${cada_pregunta}`);
+    })
     pedir_notas();
     alert(`Nota final: ${promedio_notas}`)
-    if(promedio_notas >= 7){
-    alert("Alumno aprobado")
-    } else {
-    alert("Alumno desaprobado")
+    if(promedio_notas === 0) {
+        break;
     }
+    if(promedio_notas >= 7){
+    alert(`Alumno/a aprobado/a`)
+    } else {
+    alert("Alumno/a desaprobado/a")
+    }
+
+    alert("Si desea finalizar el listado, coloque 0 en todas las areas de notas")
     alert("SIGUIENTE ALUMNO")
 }
     alert("TODOS LOS ALUMNOS FUERON EVALUADOS")
